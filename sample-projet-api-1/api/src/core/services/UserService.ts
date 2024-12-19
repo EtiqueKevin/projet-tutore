3f305f4ed6a38cb976a45d833f1e5f9d011fd0e1
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-12-19 11:54:53 +0100

    fin rm ancien api

--- sample-projet-api-1/api/src/core/services/UserService.ts
+++
@@ -1,8 +0,0 @@
-import UserDTO from '../dto/UserDTO';
-
-export default class UserService {
-    public getUserById(id: string): UserDTO {
-        
-        return new UserDTO(id, 'John Doe');
-    }
-}
