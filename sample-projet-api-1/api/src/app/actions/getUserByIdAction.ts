68ee064dcafddb651dcdc39c166c02307ae20f35
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-12-19 11:46:54 +0100

    init api gestion

--- sample-projet-api-1/api/src/app/actions/getUserByIdAction.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const getUserByIdAction = async (req: Request, res: Response): Promise<void> => {
-    try {
-        res.status(200).json({ message: req.params.id });
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving users', error });
-    }
-};
