8b32c02241c9a9f08024df2b2540771a194bd8a0
Dr-J-Watson <ptibandi54@gmail.com>
2024-12-20 14:00:04 +0100

    sql

--- api-db/sql/init.sql
+++ api-db/sql/init.sql
@@ -7,8 +7,8 @@ CREATE TABLE Users (
     Linkpic VARCHAR(255),
     Email VARCHAR(100) NOT NULL UNIQUE,
     Password VARCHAR(255) NOT NULL,
-    DateSignup TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    DateSignin TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DateSignup DATE DEFAULT CURRENT_DATE,
+    DateSignin DATE DEFAULT CURRENT_DATE
 );
 
 -- Création de la table Module
@@ -17,7 +17,7 @@ CREATE TABLE Module (
     Id_creator UUID NOT NULL REFERENCES Users(UUID) ON DELETE CASCADE,
     Description TEXT,
     Nblesson INTEGER CHECK (Nblesson >= 0),
-    DateUpdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DateUpdate DATE DEFAULT CURRENT_DATE
 );
 
 -- Création de la table Lesson
@@ -26,7 +26,7 @@ CREATE TABLE Lesson (
     Name VARCHAR(100) NOT NULL,
     Type VARCHAR(50),
     Href VARCHAR(255),
-    DateUpdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DateUpdate DATE DEFAULT CURRENT_DATE
 );
 
 -- Création de la table Module_lesson
@@ -44,7 +44,7 @@ CREATE TABLE Module_users (
     Stage INTEGER CHECK (Stage >= 0),
     Status VARCHAR(50),
     Rate INTEGER CHECK (Rate >= 0 AND Rate <= 5),
-    DateUpdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DateUpdate DATE DEFAULT CURRENT_DATE
 );
 
 -- Création de la table Exercise
@@ -52,14 +52,14 @@ CREATE TABLE Exercise (
     UUID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Id_user UUID NOT NULL REFERENCES Users(UUID) ON DELETE CASCADE,
     Href VARCHAR(255)
-)
+);
 
 -- Création de la table Comments
 CREATE TABLE Comments (
     UUID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Id_user UUID NOT NULL REFERENCES Users(UUID) ON DELETE CASCADE,
     Id_rep UUID REFERENCES Comments(UUID) ON DELETE CASCADE,
-    DatePost TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    DatePost DATE DEFAULT CURRENT_DATE,
     Content TEXT NOT NULL
 );
 
@@ -71,21 +71,18 @@ CREATE TABLE Reporting (
     Code VARCHAR(10),
     Description TEXT,
     Status VARCHAR(50),
-    DateReport TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DateReport DATE DEFAULT CURRENT_DATE
 );
-
 
 INSERT INTO Users (UUID, Name, Surname, Role, Email, Password) VALUES 
 ('3577a401-61d8-4dd1-92df-c94180075a83', 'Kevin', 'Etique', '100', 'patate@gmail.com', '$2y$10$iSP91gYLEZh5uEV0JfQiyO8CosNQ8YNLjHF7wJBuJh3mmMz0LDWX2'),
 ('235aa205-3ac9-45ed-ae89-b5590a25b183', 'Clement', 'Netange', '10', 'salope@gmail.com', '$2y$10$iSP91gYLEZh5uEV0JfQiyO8CosNQ8YNLjHF7wJBuJh3mmMz0LDWX2'),
 ('ffa99558-ee19-4877-82e5-b72500464a97', 'Amaury', 'Quilliec', '50', 'ok@gmail.com', '$2y$10$iSP91gYLEZh5uEV0JfQiyO8CosNQ8YNLjHF7wJBuJh3mmMz0LDWX2');
 
-
 INSERT INTO Module (UUID, Id_creator, Description, Nblesson) VALUES
 ('d904f66c-9ebe-4e79-b1ca-35a5802c55bb','3577a401-61d8-4dd1-92df-c94180075a83','En vrai faites pas attention',2),
 ('e8ecfee8-12d2-457f-9da2-686f0def3bda','ffa99558-ee19-4877-82e5-b72500464a97','franchement symap le cours',1),
 ('f186fb52-ae93-41ee-8c62-947f5ba35a53','ffa99558-ee19-4877-82e5-b72500464a97','AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAh',3);
-
 
 INSERT INTO Lesson (UUID, Name, Type, Href) VALUES
 ('b6bfd662-6d81-4b3b-ba3d-177ea84095b0', 'Cours 1', 'XML', ''),
@@ -108,6 +105,9 @@ INSERT INTO Module_users (UUID, Id_module, Id_users, Stage, Status, Rate) VALUES
 ('17d83d28-2ce2-4b27-bc4a-61bc7da598c5','e8ecfee8-12d2-457f-9da2-686f0def3bda','235aa205-3ac9-45ed-ae89-b5590a25b183',1,'START',NULL),
 ('a28fcfdc-afd3-440b-bc41-68d91a9222e2','f186fb52-ae93-41ee-8c62-947f5ba35a53','235aa205-3ac9-45ed-ae89-b5590a25b183',2,'START',NULL);
 
+-- CODE REPORT SIGNIFICATION
+-- Commence par 1 : Signalement d'un utilisateur
+    -- Suivi de 1 : Signalement pour usurpation
 
 -- CODE REPORT SIGNIFICATION
 -- Commence par 1 : Signalement d'un utilisateur
