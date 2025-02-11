21bd26f23a5c43a9eb741009fb3b9b08288eb2f1
Dr-J-Watson <ptibandi54@gmail.com>
2025-02-11 15:04:48 +0100

    fix docker sql

--- api-auth/sql/init.sql
+++ api-auth/sql/init.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE IF EXISTS "users";
 CREATE TABLE "public"."users" (
     "id" uuid DEFAULT uuid_generate_v4() NOT NULL,
-    "role" smallint NOT NULL,
+    "role" smallint DEFAULT 0 NOT NULL,
     "email" character varying(100) NOT NULL,
     "password" character varying(255) NOT NULL,
     "date_signup" date DEFAULT CURRENT_DATE,
