7cddf8d1f0b1c65d8c8795902bb986b88a6c8907
clem-png <clement.netange6@etu.univ-lorraine.fr>
2025-02-14 11:37:39 +0100

    fix DTO File

--- api-cours/app/src/core/dto/lesson/FileDTO.php
+++ api-cours/app/src/core/dto/lesson/FileDTO.php
@@ -9,17 +9,20 @@ class FileDTO extends DTO
     public string $type;
     public string $language;
     public string $content;
+    public string $filename;
 
-    public function __construct(string $type, string $language, string $content){
+    public function __construct(string $type,string $filename, string $language, string $content){
         $this->type = $type;
         $this->language = $language;
         $this->content = $content;
+        $this->filename = $filename;
     }
 
     public function jsonSerialize(): array
     {
         return [
             'type' => $this->type,
+            'filename' => $this->filename,
             'language' => $this->language,
             'content' => $this->content,
         ];
