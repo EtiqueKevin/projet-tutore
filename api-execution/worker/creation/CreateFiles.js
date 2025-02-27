0d94c09479ea1e15b757ed20858bc833bb8fd4d8
TyrYoxan <clem.bri25@gmail.com>
2025-02-27 09:56:52 +0100

    Fix String

--- api-execution/worker/creation/CreateFiles.js
+++ api-execution/worker/creation/CreateFiles.js
@@ -5,9 +5,9 @@ function createFiles(file, code) {
     code.codes.forEach(code_info => {
         const fileName = code_info.fileName;
         const code = code_info.code;
-        fileCreationCommands += `echo "${code}" > ${file}/${fileName} && `;
+        fileCreationCommands += `echo "${code.replace(/(["\\$`'])/g, '\\$1')}" > ${file}/${fileName} && `;
     });
 
-    return fileCreationCommands += `echo "${code.testCode}" > ${file}/${code.fileTest} `;
+    return fileCreationCommands += `echo "${code.testCode.replace(/(["\\$`'])/g, '\\$1')}" > ${file}/${code.fileTest} `;
 }
 module.exports = createFiles;
